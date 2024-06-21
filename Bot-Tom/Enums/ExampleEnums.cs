cc8968057b2aa2f8652f354b82b419407c024e05
SEE YOU SPACE BUNNY <ld.0x29a@gmail.com>
2024-06-21 15:12:01 +0200

    new branch trying interaction

---
+++ Bot-Tom/Enums/ExampleEnums.cs
@@ -0,0 +1,13 @@
+using Discord.Interactions;
+
+namespace BotTom;
+
+public enum ExampleEnum
+{
+    First,
+    Second,
+    Third,
+    Fourth,
+    [ChoiceDisplay("Twenty First")]
+    TwentyFirst
+}
